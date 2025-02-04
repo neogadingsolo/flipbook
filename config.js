55faff99560e1d07d43592cdacb7dc5e73d63517
neogadingsolo <gadingsoloit@neohotels.com>
2025-02-04 11:31:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+var pdfFiles = [
+  'pdf/food_menu.pdf',
+  'pdf/beverage_menu.pdf',
+  'pdf/kids_menu.pdf'
+];
+
+var flipbookOptions = {
+  height: 2000,
+  duration: 700,
+  backgroundColor: "#000000",
+  soundEnable: true,
+  useTouch: true
+};
